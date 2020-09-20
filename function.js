bcb660ddbf3239584635c4fe64bdebc02e30d3fa
RottenMan <1054510164@qq.com>
2020-09-20 14:17:01 +0800

    new

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ var express = require("express");
 // const axios = require("axios");
 
 const utils = require("./utils");
-
+const bodyParser = require("body-parser");
 const router = express.Router();
 // const fs = require("fs");
 /**
@@ -28,48 +28,30 @@ function returnData(code, message, data = {}) {
 // let numberOfDraws = 3;
 let nowadays = utils.nowDay();
 router.get("/prizeInformation", (req, res) => {
-  // let numberOfDraws = 0;
-  // let phone = req.session.phone;
-  // if (req.userData[phone][nowadays] === undefined) {
-  //   req.userData[phone][nowadays] = [];
-  //   utils.writeFile("user.json", req.userData);
-  //   numberOfDraws = 3 - req.userData[phone][nowadays].length;
-  // } else {
-  //   numberOfDraws = 3 - req.userData[phone][nowadays].length;
-  // }
   res.send(
     returnData(0, "抽奖信息", {
       commodity: req.goodsData,
-      frequency: numberOfDraws,
     })
   );
 });
 
 // 点击抽奖
 router.post("/lottery", (req, res) => {
-  let prize = req.body.prize;
-  prize = JSON.parse(prize);
-  // let phone = req.body.phone;
-  // let {prize,phone} = req.body;
-  console.log(typeof prize);
-  let phone = "18830705516";
-  // let phone = req.session.phone;
+  let prize = JSON.parse(req.body.prize);
+  let phone = req.body.phone;
   let numberOfDraws = 2 - req.userData[phone][nowadays].length;
-  // console.log(numberOfDraws);
   if (numberOfDraws <= 0) {
     res.send(returnData(1, "抽奖次数已用完"));
   } else {
-    // let prizeName = utils.randomDraw(req.goodsData);
     req.userData[phone][nowadays].push(prize.prizeName);
     utils.writeFile("user.json", req.userData);
     --numberOfDraws;
-    res.send(returnData(0, "抽奖成功", { prizeName }));
+    res.send(returnData(0, "抽奖成功", {}));
   }
 });
 
 // 返回所有的抽奖结果
 router.get("/checkLotteryResults", (req, res) => {
-  // let phone = "18830705516";
   let phone = req.session.phone;
   console.log("1");
   if (req.userData[phone]) {
